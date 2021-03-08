448d19855440439e9a4c51c03fdecb8de489327f
GoncaloOlas <goncalo.olas@gmail.com>
2021-03-08 18:41:30 +0000

    Added timer skeleton

--- myBuzzer.py
+++ myBuzzer.py
@@ -1,7 +1,24 @@
 #Start timer function
+import time
 
-ok = 0
+print('''This is my timer, we will come back to this and turn it into a beautiful app 
+                        or window, for now we get this...''')
 
-cool = 0
+#variables
 
-#SOME CODE
+seconds = 0
+minutes = 0
+
+#Ask to begin
+start = input("Lets start, shall we? (y/n)")
+
+if start == y:
+    loop = 1
+
+while loop = 1 and minutes < 90:
+    seconds +=1
+    print(str(minutes) + " mins " + str(seconds) + " secs")
+    if seconds = 60:
+        seconds = 0
+        minutes += 1
+    print(str(minutes) + " mins " + str(seconds) + " secs")
